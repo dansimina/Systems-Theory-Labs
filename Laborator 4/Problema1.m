6a8332fdbdc6a43895d213c558a4bbca9a75fb7c
Simina Dan-Marius <dansimina10@gmail.com>
2024-04-12 12:16:07 +0300

    lab4

---
+++ Laborator 4/Problema1.m
@@ -0,0 +1,11 @@
+% 1.
+% Gc = 2;
+% G = tf(10, [1 10]);
+% G0 = feedback(Gc * G, 1); %reactie
+% step(G0)
+
+% 2.
+% % Gc = tf([2 20], [1 0]);
+% % G = tf(10, [1 10]);
+% % G0 = feedback(Gc * G, 1); %reactie
+% % step(G0)
